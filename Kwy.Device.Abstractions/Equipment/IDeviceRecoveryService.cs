9bd75ef12e9e73efbcda217c605a7c076eb171eb
Kawayi.cn <kawayi.cn@outlook.com>
2026-09-23 20:22:30 +0800

    Equipment层移除

--- Kwy.Device.Abstractions/Equipment/IDeviceRecoveryService.cs
+++
@@ -1,8 +0,0 @@
-namespace Kwy.Device.Abstractions.Equipment;
-
-public interface IDeviceRecoveryService
-{
-    Task<DeviceRecoveryResult> RecoverAsync(
-        DeviceRecoveryContext context,
-        CancellationToken cancellationToken = default);
-}
--- Kwy.Device.Abstractions/Equipment/IDeviceStateSynchronizer.cs
+++
@@ -1,6 +0,0 @@
-namespace Kwy.Device.Abstractions.Equipment;
-
-public interface IDeviceStateSynchronizer
-{
-    Task<DeviceSyncResult> SyncStateAsync(CancellationToken cancellationToken = default);
-}
